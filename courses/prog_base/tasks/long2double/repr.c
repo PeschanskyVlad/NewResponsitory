fa3f32204442cb64b56013063db4cba107c755cb
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2015-10-11 23:27:27 +0300

    c

---
+++ courses/prog_base/tasks/long2double/repr.c
@@ -0,0 +1,72 @@
+#include <math.h>
+double long2double(long long num){
+
+   long long num1;
+    int a[64];
+    int b[52];
+    int c[11];
+    int i,ress = 0;
+    double ste =0;
+    double res =0;
+
+
+
+   for(i=0;i<64;i++){
+    a[i]=0;
+    }
+if (num<0){
+unsigned long long num1 = num +  9223372036854775808LL;
+a[0] = 1;
+for(i=63;i>0;i--){
+    a[i]=num1%2;
+    num1=num1/2;
+}
+}
+else{
+a[0] = 0;
+
+    for(i=63;i>0;i--){
+    a[i]=num%2;
+    num=num/2;
+}
+}
+for(i=11;i>0;i--){
+ste=ste+a[i]*pow(2,11-i);
+}
+
+
+if (ste == 0){
+for (i = 12; i < 64; i++){
+res = res + a[i] * pow(2, (-i + 11) + (-1022));
+}
+} else if (ste == 2047){
+for(i=63;i>0;i--){
+if(a[i]==1){
+ress=1;
+break;
+}
+if(ress>0){
+res=NAN;
+}
+}
+if(a[0]==1&&ress==0){
+res=-INFINITY;
+}
+if(a[0]==0&&ress==0){
+res=+INFINITY;
+}
+
+
+} else{
+for (i = 12; i < 64; i++){
+res = res + a[i] * pow(2, (-i + 11) + (ste - 1023));
+}
+res+= pow(2, (ste - 1023));
+
+}
+
+return res;
+
+
+
+}
