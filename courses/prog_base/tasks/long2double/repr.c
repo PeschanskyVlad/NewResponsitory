a7a8e05a25d903a8228d1cb5389daa169abfe73d
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2015-10-12 07:55:21 +0300

    c

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -3,8 +3,6 @@ double long2double(long long num){
 
    long long num1;
     int a[64];
-    int b[52];
-    int c[11];
     int i,ress = 0;
     double ste =0;
     double res =0;
@@ -14,22 +12,23 @@ double long2double(long long num){
    for(i=0;i<64;i++){
     a[i]=0;
     }
-if (num<0){
-unsigned long long num1 = num +  9223372036854775808LL;
-a[0] = 1;
-for(i=63;i>0;i--){
-    a[i]=num1%2;
-    num1=num1/2;
-}
-}
-else{
-a[0] = 0;
 
-    for(i=63;i>0;i--){
-    a[i]=num%2;
-    num=num/2;
+	if (num<0){
+	unsigned long long num1 = num +  9223372036854775808LL;
+	a[0] = 1;
+	for(i=63;i>0;i--){
+    		a[i]=num1%2;
+    		num1=num1/2;
 }
 }
+	else{
+	a[0] = 0;
+		for(i=63;i>0;i--){
+    		a[i]=num%2;
+    		num=num/2;
+}
+}
+
 for(i=11;i>0;i--){
 ste=ste+a[i]*pow(2,11-i);
 }
@@ -50,7 +49,9 @@ ress=1;
 break;
 }
 }
+
 res= ress ? NAN : INFINITY;
+
 } else{
 for (i = 12; i < 64; i++){
 res = res + a[i] * pow(2, (-i + 11) + (ste - 1023));
