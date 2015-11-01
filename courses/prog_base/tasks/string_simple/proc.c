3eebf9e74460d5dab1ffb4eafcd0b27ad2acca18
PeschanskyVlad <kpi.kp52.peschansky@gmail.com>
2015-11-01 21:05:59 +0200

    c

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -1,6 +1,7 @@
 char * process(char * resultStr, const char * textLines[],
                int linesNum, const char * extraStr) {
     int i,j,r=0,r1=0;
+char  Alf[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
 
 
     for (j=0;j<linesNum;j++){
